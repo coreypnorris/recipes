4cf0b862d7962f13f93810fdab0e68395dea5f22
Corey Norris & Bailey Drake <student@epicodus.com>
2014-04-03 10:31:46 -0700

    CRUD/L for Recipes

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Recipes::Application.config.secret_key_base = '410319014f5f5fcbb83fff5101cc7c31698a5e8b7d77885ff10681b105709855388fec8e35abe23ce0bed4a99a2c1466bc3ef892d98ce6998df1c7e892e87ca2'
---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Recipes::Application.routes.draw do
+  match('recipes', {:via => :get, :to => 'recipes#index'})
+  match('recipes', {:via => :post, :to => 'recipes#create'})
+  match('recipes/new', {:via => :get, :to => 'recipes#new'})
+  match('recipes/:id', {:via => :get, :to => 'recipes#show'})
+  match('recipes/:id', {:via => [:patch, :put], :to => 'recipes#update'})
+  match('recipes/:id/edit', {:via => :get, :to => 'recipes#edit'})
+  match('recipes/:id', {:via => :delete, :to => 'recipes#destroy'})
+end
